e7162904cdac2fae41114c8ed4a15e240da327aa
Megha Banerjee <mbanerjee@opentext.com>
2023-03-08 01:47:28 +0530

    employee code set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { LogoutPageComponent } from './logout-page/logout-page.component';
+import { EmployeeListPageComponent } from './employee-list-page/employee-list-page.component';
+import { HeaderComponent } from './header/header.component';
+import { EmployeeDetailsPageComponent } from './employee-details-page/employee-details-page.component';
+import { EmployeeCardComponent } from './employee-card/employee-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    LogoutPageComponent,
+    EmployeeListPageComponent,
+    HeaderComponent,
+    EmployeeDetailsPageComponent,
+    EmployeeCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/employee-details-page/employee-details-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-details-page',
+  templateUrl: './employee-details-page.component.html',
+  styleUrls: ['./employee-details-page.component.scss']
+})
+export class EmployeeDetailsPageComponent {
+
+}
